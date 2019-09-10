2399ad28736a3794ffc0a18649906019dc1a4f40
Andrew Booze <42682253+awbooze@users.noreply.github.com>
2019-09-10 13:50:27 -0500

    Add Pterodactyl Wings

---
+++ Menu/Entrees/PterodactylWings.cs
@@ -0,0 +1,46 @@
+﻿/*  PterodcatylWings.cs
+*   Author: Andrew Booze
+*/
+
+using System.Collections.Generic;
+
+namespace DinoDiner.Menu.Entrees
+{
+    /// <summary>
+    /// A class that represents an entree of Pterodactyl Wings.
+    /// </summary>
+    public class PterodactylWings
+    {
+        /// <summary>
+        /// The price of this entree.
+        /// </summary>
+        public double Price { get; set; }
+        
+        /// <summary>
+        /// The number of calories in this entree.
+        /// </summary>
+        public uint Calories { get; set; }
+
+        /// <summary>
+        /// The list of ingredients for this entree.
+        /// </summary>
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Chicken" };
+                ingredients.Add("Wing Sauce");
+                return ingredients;
+            }
+        }
+
+        /// <summary>
+        /// The constructor for this entree. Sets the price and the calories.
+        /// </summary>
+        public PterodactylWings()
+        {
+            this.Price = 7.21;
+            this.Calories = 318;
+        }
+    }
+}
