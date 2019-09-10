c7af3d9d9e31d916a09298dd6ecffb0258a92b14
Andrew Booze <42682253+awbooze@users.noreply.github.com>
2019-09-10 14:23:59 -0500

    Add T-Rex King Burger entree

---
+++ Menu/Entrees/TRexKingBurger.cs
@@ -0,0 +1,157 @@
+﻿/*  TRexKingBurger.cs
+*   Author: Andrew Booze
+*/
+
+using System.Collections.Generic;
+
+namespace DinoDiner.Menu.Entrees
+{
+    /// <summary>
+    /// A class that represents a T-Rex King Burger entree.
+    /// </summary>
+    public class TRexKingBurger
+    {
+        /// <summary>
+        /// Whether or not the bun is included in this entree.
+        /// </summary>
+        private bool bun = true;
+
+        /// <summary>
+        /// Whether or not the lettuce is included in this entree.
+        /// </summary>
+        private bool lettuce = true;
+
+        /// <summary>
+        /// Whether or not the tomato is included in this entree.
+        /// </summary>
+        private bool tomato = true;
+
+        /// <summary>
+        /// Whether or not the onion is included in this entree.
+        /// </summary>
+        private bool onion = true;
+
+        /// <summary>
+        /// Whether or not the pickle is included in this entree.
+        /// </summary>
+        private bool pickle = true;
+
+        /// <summary>
+        /// Whether or not the ketchup is included in this entree.
+        /// </summary>
+        private bool ketchup = true;
+
+        /// <summary>
+        /// Whether or not the mustard is included in this entree.
+        /// </summary>
+        private bool mustard = true;
+
+        /// <summary>
+        /// Whether or not the mayo is included in this entree.
+        /// </summary>
+        private bool mayo = true;
+
+        /// <summary>
+        /// The price of this entree.
+        /// </summary>
+        public double Price { get; set; }
+        
+        /// <summary>
+        /// The number of calories in this entree.
+        /// </summary>
+        public uint Calories { get; set; }
+
+        /// <summary>
+        /// The list of ingredients for this entree.
+        /// </summary>
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Steakburger Pattie", "Steakburger Pattie", "Steakburger Pattie" };
+                if (bun) ingredients.Add("Whole Wheat Bun");
+                if (lettuce) ingredients.Add("Lettuce");
+                if (tomato) ingredients.Add("Tomato");
+                if (onion) ingredients.Add("Onion");
+                if (pickle) ingredients.Add("Pickle");
+                if (ketchup) ingredients.Add("Ketchup");
+                if (mustard) ingredients.Add("Mustard");
+                if (mayo) ingredients.Add("Mayo");
+                return ingredients;
+            }
+        }
+
+        /// <summary>
+        /// The constructor for this entree. Sets the price and the calories.
+        /// </summary>
+        public TRexKingBurger()
+        {
+            this.Price = 8.45;
+            this.Calories = 728;
+        }
+
+        /// <summary>
+        /// Allows the user to remove the bun from this entree.
+        /// </summary>
+        public void HoldBun()
+        {
+            this.bun = false;
+        }
+
+        /// <summary>
+        /// Allows the user to remove the lettuce from this entree.
+        /// </summary>
+        public void HoldLettuce()
+        {
+            this.lettuce = false;
+        }
+
+        /// <summary>
+        /// Allows the user to remove the tomato from this entree.
+        /// </summary>
+        public void HoldTomato()
+        {
+            this.tomato = false;
+        }
+
+        /// <summary>
+        /// Allows the user to remove the onion from this entree.
+        /// </summary>
+        public void HoldOnion()
+        {
+            this.onion = false;
+        }
+
+        /// <summary>
+        /// Allows the user to remove the pickle from this entree.
+        /// </summary>
+        public void HoldPickle()
+        {
+            this.pickle = false;
+        }
+
+        /// <summary>
+        /// Allows the user to remove the ketchup from this entree.
+        /// </summary>
+        public void HoldKetchup()
+        {
+            this.ketchup = false;
+        }
+
+        /// <summary>
+        /// Allows the user to remove the mustard from this entree.
+        /// </summary>
+        public void HoldMustard()
+        {
+            this.mustard = false;
+        }
+
+        /// <summary>
+        /// Allows the user to remove the mayo from this entree.
+        /// </summary>
+        public void HoldMayo()
+        {
+            this.mayo = false;
+        }
+    }
+}
