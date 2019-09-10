04d8cdbfd5053c387371cc3bf3e2edc3163386c7
Andrew Booze <42682253+awbooze@users.noreply.github.com>
2019-09-10 14:42:24 -0500

    Add Dino Nuggets entree and fix test errors and line endings

--- Menu/Entrees/Brontowurst.cs
+++ Menu/Entrees/Brontowurst.cs
@@ -46,7 +46,7 @@ namespace DinoDiner.Menu.Entrees
                 List<string> ingredients = new List<string>() { "Brautwurst" };
                 if (bun) ingredients.Add("Whole Wheat Bun");
                 if (peppers) ingredients.Add("Peppers");
-                if (onions) ingredients.Add("Onions");
+                if (onions) ingredients.Add("Onion");
                 return ingredients;
             }
         }
---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,60 @@
+﻿/*  DinoNuggets.cs
+*   Author: Andrew Booze
+*/
+
+using System.Collections.Generic;
+
+namespace DinoDiner.Menu.Entrees
+{
+    /// <summary>
+    /// A class that represents an entree of Dino Nuggets.
+    /// </summary>
+    public class DinoNuggets
+    {
+        private uint numberOfNuggets;
+
+        /// <summary>
+        /// The price of this entree.
+        /// </summary>
+        public double Price { get; set; }
+        
+        /// <summary>
+        /// The number of calories in this entree.
+        /// </summary>
+        public uint Calories { get; set; }
+
+        /// <summary>
+        /// The list of ingredients for this entree.
+        /// </summary>
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { };
+                for (int i = 0; i < numberOfNuggets; i++)
+                {
+                    ingredients.Add("Chicken Nugget");
+                }
+
+                return ingredients;
+            }
+        }
+
+        /// <summary>
+        /// The constructor for this entree. Sets the price and the calories.
+        /// </summary>
+        public DinoNuggets()
+        {
+            this.numberOfNuggets = 6;
+            this.Price = 4.25;
+            this.Calories = 59 * numberOfNuggets;
+        }
+
+        public void AddNugget()
+        {
+            numberOfNuggets++;
+            this.Price += 0.25;
+            this.Calories += 59;
+        }
+    }
+}
