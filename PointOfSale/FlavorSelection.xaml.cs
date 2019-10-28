6db8690b59bb3fbc81f8b54014a895f17a05f589
Andrew Booze <42682253+awbooze@users.noreply.github.com>
2019-10-27 23:04:07 -0500

    Add ability to add and remove flavors to sodasaurus drinks

--- PointOfSale/FlavorSelection.xaml.cs
+++ PointOfSale/FlavorSelection.xaml.cs
@@ -7,6 +7,8 @@ using System;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
 
 namespace PointOfSale
 {
@@ -21,8 +23,89 @@ namespace PointOfSale
         public FlavorSelection()
         {
             InitializeComponent();
+        }
 
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is Order order)
+            {
+                CollectionViewSource.GetDefaultView(order.Items).CurrentChanged += DrinkSelection_CurrentChanged;
+            }
+            UpdateFlavorButtons();
+        }
+
+        // Updates flavor buttons when the current selection changes
+        private void DrinkSelection_CurrentChanged(object sender, EventArgs e)
+        {
+            UpdateFlavorButtons();
+        }
+
+        // Preforms any actions necessary when a flavor button is clicked
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
             // Adds buttons for each flavor to the menu programatically.
+            List<SodasaurusFlavor> sodasaurusFlavors = new List<SodasaurusFlavor>
+            {
+                SodasaurusFlavor.Cola,
+                SodasaurusFlavor.Orange,
+                SodasaurusFlavor.Vanilla,
+                SodasaurusFlavor.Chocolate,
+                SodasaurusFlavor.RootBeer,
+                SodasaurusFlavor.Cherry,
+                SodasaurusFlavor.Lime,
+                SodasaurusFlavor.Grape
+            };
+
+            if (sender is Button button && DataContext is Order order && 
+                CollectionViewSource.GetDefaultView(order.Items).CurrentItem is Sodasaurus soda)
+            {
+                if (button.Name == App.CreateValidIdString(SodasaurusFlavor.Cola.ToString()))
+                {
+                    // Swaps the flavor: If it already exists then the operation removes it, otherwise, 
+                    // the operation adds it.
+                    soda.Flavor ^= SodasaurusFlavor.Cola;
+                }
+                else if (button.Name == App.CreateValidIdString(SodasaurusFlavor.Orange.ToString()))
+                {
+                    soda.Flavor ^= SodasaurusFlavor.Orange;
+                }
+                else if (button.Name == App.CreateValidIdString(SodasaurusFlavor.Vanilla.ToString()))
+                {
+                    soda.Flavor ^= SodasaurusFlavor.Vanilla;
+                }
+                else if (button.Name == App.CreateValidIdString(SodasaurusFlavor.Chocolate.ToString()))
+                {
+                    soda.Flavor ^= SodasaurusFlavor.Chocolate;
+                }
+                else if (button.Name == App.CreateValidIdString(SodasaurusFlavor.RootBeer.ToString()))
+                {
+                    soda.Flavor ^= SodasaurusFlavor.RootBeer;
+                }
+                else if (button.Name == App.CreateValidIdString(SodasaurusFlavor.Cherry.ToString()))
+                {
+                    soda.Flavor ^= SodasaurusFlavor.Cherry;
+                }
+                else if (button.Name == App.CreateValidIdString(SodasaurusFlavor.Lime.ToString()))
+                {
+                    soda.Flavor ^= SodasaurusFlavor.Lime;
+                }
+                else if (button.Name == App.CreateValidIdString(SodasaurusFlavor.Grape.ToString()))
+                {
+                    soda.Flavor ^= SodasaurusFlavor.Grape;
+                }
+            }
+
+            UpdateFlavorButtons();
+        }
+
+        // Adds buttons for each flavor to the menu programatically.
+        private void UpdateFlavorButtons()
+        {
+            while (FlavorGrid.Children.Count > 0)
+            {
+                FlavorGrid.Children.RemoveAt(0);
+            }
+
             List<SodasaurusFlavor> sodasaurusFlavors = new List<SodasaurusFlavor>
             {
                 SodasaurusFlavor.Cola,
@@ -61,15 +144,16 @@ namespace PointOfSale
                     x = 0;
                 }
 
+                if (DataContext is Order order &&
+                    CollectionViewSource.GetDefaultView(order.Items).CurrentItem is Sodasaurus soda &&
+                    soda.Flavor.HasFlag(flavor))
+                {
+                    button.Background = Brushes.LightSteelBlue;
+                }
+
                 button.Click += new RoutedEventHandler(Button_Click);
                 FlavorGrid.Children.Add(button);
             }
-        }
-
-        // Preforms any actions necessary when a flavor button is clicked
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
         }
 
         // Returns to the DrinkSelection screen when clicked.
