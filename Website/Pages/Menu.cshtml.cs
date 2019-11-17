0736127c569bb1a19876c5451c216bcef7d459cc
Andrew Booze <42682253+awbooze@users.noreply.github.com>
2019-11-16 20:50:01 -0600

    Finalize display of menu items for this milestone

--- Website/Pages/Menu.cshtml.cs
+++ Website/Pages/Menu.cshtml.cs
@@ -1,26 +1,15 @@
 ﻿using DinoDiner.Menu;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Collections.Generic;
 
 namespace Website.Pages
 {
     public class MenuModel : PageModel
     {
-        public List<CretaceousCombo> ComboList { get; private set; }
-
-        public List<Entree> EntreeList { get; private set; }
-
-        public List<Side> SideList { get; private set; }
-
-        public List<Drink> DrinkList { get; private set; }
+        public Menu Menu { get; private set; }
 
         public void OnGet()
         {
-            Menu menu = new Menu();
-            ComboList = menu.AvailableCombos;
-            EntreeList = menu.AvailableEntrees;
-            SideList = menu.AvailableSides;
-            DrinkList = menu.AvailableDrinks;
+            Menu = new Menu();
         }
     }
 }
