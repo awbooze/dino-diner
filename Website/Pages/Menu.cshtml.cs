267646b3882d9d1af1a811fe346fb7eb7094037b
Andrew Booze <42682253+awbooze@users.noreply.github.com>
2019-11-16 19:44:33 -0600

    Add Menu page and rudimentary combo display

---
+++ Website/Pages/Menu.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using DinoDiner.Menu;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace Website.Pages
+{
+    public class MenuModel : PageModel
+    {
+        public List<CretaceousCombo> ComboList { get; private set; }
+
+        public List<Entree> EntreeList { get; private set; }
+
+        public List<Side> SideList { get; private set; }
+
+        public List<Drink> DrinkList { get; private set; }
+
+        public void OnGet()
+        {
+            Menu menu = new Menu();
+            ComboList = menu.AvailableCombos;
+            EntreeList = menu.AvailableEntrees;
+            SideList = menu.AvailableSides;
+            DrinkList = menu.AvailableDrinks;
+        }
+    }
+}
