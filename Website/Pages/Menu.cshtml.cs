700585c5106579ff94326385ff136e5ed11f2ad0
Andrew Booze <42682253+awbooze@users.noreply.github.com>
2019-11-25 17:10:59 -0600

    Added BindProperties and rudamentary OnPost method for sorting and filtering

--- Website/Pages/Menu.cshtml.cs
+++ Website/Pages/Menu.cshtml.cs
@@ -3,7 +3,9 @@
 */
 
 using DinoDiner.Menu;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
 namespace Website.Pages
 {
@@ -15,7 +17,37 @@ namespace Website.Pages
         /// <summary>
         /// A reference to the Menu object created by the OnGet() method and set through the private setter.
         /// </summary>
-        public Menu Menu { get; private set; }
+        public Menu Menu { get; private set; } = new Menu();
+
+        /// <summary>
+        /// The search term when a search is executed through the search and filter button.
+        /// </summary>
+        [BindProperty]
+        public string search { get; set; }
+
+        /// <summary>
+        /// A list of food categories set when a search is executed.
+        /// </summary>
+        [BindProperty]
+        public List<string> menuCategory { get; set; } = new List<string>();
+
+        /// <summary>
+        /// A nullable minimum price set when a search is executed.
+        /// </summary>
+        [BindProperty]
+        public float? minimumPrice { get; set; }
+
+        /// <summary>
+        /// A nullable maximum price set when a search is executed.
+        /// </summary>
+        [BindProperty]
+        public float? maximumPrice { get; set; }
+
+        /// <summary>
+        /// A list of excluded ingredients set when a search is executed.
+        /// </summary>
+        [BindProperty]
+        public List<string> excludedIngredients { get; set; } = new List<string>();
 
         /// <summary>
         /// Method executed whenever a get request is recieved to get the current page. 
@@ -23,7 +55,16 @@ namespace Website.Pages
         /// </summary>
         public void OnGet()
         {
-            Menu = new Menu();
+            
+        }
+
+        /// <summary>
+        /// Method executed whenever a post request is recieved for the current page. Searches and filters 
+        /// the menu items on the page and then returns the ones that match the filters.
+        /// </summary>
+        public void OnPost()
+        {
+            
         }
     }
 }
